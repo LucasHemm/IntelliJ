7815fee518e4364170af59b7ed039890d6085ca8
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-03-03 11:49:54 +0100

    task1 done

--- torsdagsopgaver5/Tasks/src/com/company/Main.java
+++ torsdagsopgaver5/Tasks/src/com/company/Main.java
@@ -1,8 +1,79 @@
 package com.company;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class Main {
+public class Main{
+    static Scanner scan;
+    private static String[] text;
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException{
+        File file = new File("data.txt");
+        scan = new Scanner(file);
+
+        String inputFromFile = "";
+
+        while (scan.hasNextLine())                                // checks if theres more lines in the file
+        {
+            inputFromFile += scan.nextLine();                     // adds each line to the inputFromFile string.
+        }
+
+        text = inputFromFile.split(" ");                    // Creates and array of strings, where each element is a single word from the file.
+        System.out.println(text.length);
+
+        //printWordsStartingWith("Ø");
+
+        //printWordsOfLength(3);
+
+
+        //test dine metoder ved at kalde dem her:
+        printLongestWord();
+
     }
+
+    private static void printWordsOfLength(int l)
+    {
+        boolean wordisvalid = true;
+
+        for (String s : text)
+        {
+            if (s.length() == l)
+            {
+                if (s.contains(",") || s.contains("."))
+                {
+                    wordisvalid = false;
+                }
+
+                if (wordisvalid)
+                {
+                    System.out.println(s);
+                }
+            }
+        }
+    }
+
+    private static void printWordsStartingWith(String pattern)
+    {
+        for (String s : text) // for each word in text
+        {
+            if (s.startsWith(pattern) || s.startsWith(pattern.toLowerCase()))
+            {
+                System.out.println(s);
+            }
+        }
+    }
+    //skriv dine metoder herunder:
+    private static void printLongestWord(){
+        int counter = 0;
+        String longestWord ="";
+        for(String s : text){
+            if(s.length() > counter){
+                counter = s.length();
+               longestWord = s;
+            }
+        }
+        System.out.println(longestWord);
+    }
+
+
 }
