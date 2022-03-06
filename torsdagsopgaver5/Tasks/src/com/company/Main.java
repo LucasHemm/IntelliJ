a17c58aecd4081e94f66b3e895bfbfc540124b3b
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-03-06 19:00:00 +0100

    done

--- torsdagsopgaver5/Tasks/src/com/company/Main.java
+++ torsdagsopgaver5/Tasks/src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main{
@@ -19,7 +20,7 @@ public class Main{
         }
 
         text = inputFromFile.split(" ");                    // Creates and array of strings, where each element is a single word from the file.
-        System.out.println(text.length);
+        //System.out.println(text.length);
 
         //printWordsStartingWith("Ø");
 
@@ -28,8 +29,8 @@ public class Main{
 
         //test dine metoder ved at kalde dem her:
         //printLongestWord();
-        printFirstHalfOfEachWord();
-
+        //printFirstHalfOfEachWord();
+        printMostFrequentLetter();
 
     }
 
@@ -83,10 +84,85 @@ public class Main{
                 System.out.println(s.substring(0,s.length()/2));
             }
         }
+    static ArrayList<Character> chars = new ArrayList<>();
+    static ArrayList<Integer> charCount = new ArrayList<>();
         //task 3
         private static void printMostFrequentLetter(){
-        
+            for(String s: text){
+                s = s.toLowerCase();
+                for(int i = 0; i <= s.length()-1;i++){
+                    char x = s.charAt(i);
+                    findChar2(x);
+                }
+
+            }
+
+            System.out.println(chars);
+           System.out.println(charCount);
+            //System.out.println(chars.size());
+
+
+        }
+
+        private static void findChar1(char x){
+            //System.out.println(x);
+           if(chars.size() == 0){
+               chars.add(x);
+               charCount.add(1);
+               System.out.println("den er nul");
+           }
+
+            for(int i = 0; i < chars.size(); i++){
+                if(chars.get(i) == x){
+                    charCount.set(i, i += 1);
+                   // System.out.println("if statement");
+                    System.out.println("den er kendt " + x);
+                }
+                else{
+                    chars.add(x);
+                    charCount.add(1);
+                    System.out.println("den er ukendt " + x);
+                    //System.out.println("else statement");
+                }
+            }
+
+           // for(int i = 0; i <= chars.size(); i++){
+        }
+
+        private static void findChar2(char x ){
+            boolean found = false;
+               if(chars.size() == 0){
+                   chars.add(x);
+                   System.out.println(chars.get(0));
+               }
+               else{
+                   for(int i = 0; i < chars.size()-1;i++){
+                       if(chars.get(i) == x){
+                           //System.out.println("if statement");
+                           System.out.println(x);
+                           found = true;
+                           System.out.println("i: " + i + " arraysize:" + chars.size());
+                           charCount.set(i, charCount.get(i)+1);
+                       }
+                       else{
+                           //System.out.println("else statement");
+                       }
+                   }
+                   if(found == false){
+                       chars.add(x);
+                       charCount.add(1);
+                       System.out.println("fandt nyt bogstav: " + x);
+                   }
+               }
         }
 
 
+
 }
+
+
+
+        /*letters.add('a'); letters.add('b'); letters.add('c'); letters.add('d'); letters.add('e'); letters.add('f');
+        letters.add('g'); letters.add('h'); letters.add('i'); letters.add('j'); letters.add('k'); letters.add('l');
+        letters.add('m'); letters.add('n'); letters.add('o'); letters.add('p'); letters.add('q'); letters.add('b');
+*/
