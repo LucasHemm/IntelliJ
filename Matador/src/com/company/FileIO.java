1b32614fa6c01066838b6918df82de2cb2ab8609
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-03-07 14:02:08 +0100

    update

--- Matador/src/com/company/FileIO.java
+++ Matador/src/com/company/FileIO.java
@@ -5,21 +5,23 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class FileIO
-{
+public class FileIO {
+    ArrayList<String> readGameData() {
+
+
     File file = new File("data.txt");
     ArrayList<String> data = new ArrayList<>();
-        try {
-    Scanner scan = new Scanner(file);
-    while(scan.hasNextLine()){
-        data.add(scan.nextLine());//“Egon: 30000”
+
+    try{
+        Scanner scan = new Scanner(file);
+        while(scan.hasNextLine()){
+            data.add(scan.nextLine());
+        }
     }
-}catch(
-    FileNotFoundException e){
-    e.printStackTrace();
-}
+    catch(FileNotFoundException e) {
+        e.printStackTrace();
+    }
         return data;
-
-
-
+    }
 }
+
--- Matador/src/com/company/Game.java
+++ Matador/src/com/company/Game.java
@@ -3,13 +3,18 @@ import java.util.ArrayList;
 
 public class Game {
     ArrayList<Player> players = new ArrayList<>();
+    FileIO fileio = new FileIO();
+
     public Game(){
+        /*
         Player player1 = new Player("Lucas", 100);
         Player player2 = new Player("Hans", 200);
         Player player3 = new Player("Peter", 300);
         players.add(player1);
         players.add(player2);
         players.add(player3);
+         */
+
     }
 
 
