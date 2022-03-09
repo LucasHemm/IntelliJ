7f3b0888cf6daa4446b3308464e125d6354bff81
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-03-09 14:50:29 +0100

    update

--- torsdagsopgaver6/src/Task2/Main.java
+++ torsdagsopgaver6/src/Task2/Main.java
@@ -1,7 +1,19 @@
 package Task2;
 
+import java.util.ArrayList;
+
 public class Main {
+    static ArrayList<Room> roomsList = new ArrayList<>();
     public static void main(String[] args){
         System.out.println("hello world");
+        Room room1 = new Room(4,2,3,4);
+        Room room2 = new Room(3,3,3,3);
+        Room room3 = new Room(4,1,2,6);
+        roomsList.add(room1);
+        roomsList.add(room2);
+        roomsList.add(room3);
+        Building building1 = new Building(roomsList,2,3,true);
+        
+
     }
 }
--- torsdagsopgaver6/src/Task2/Room.java
+++ torsdagsopgaver6/src/Task2/Room.java
@@ -1,4 +1,33 @@
 package Task2;
 
 public class Room {
+
+    private int walls;
+    private int numberOfDoors;
+    private int numberOfLamps;
+    private int numberOfWindows;
+
+    public Room(int walls, int numberOfDoor, int numberOfLamps, int numberOfWindows){
+        this.walls = walls;
+        this.numberOfDoors = numberOfDoor;
+        this.numberOfLamps = numberOfLamps;
+        this.numberOfWindows = numberOfWindows;
+    }
+
+    public int getWalls(){
+        return this.walls;
+    }
+    public int getNumberOfDoors(){
+        return numberOfDoors;
+    }
+    public int getNumberOfLamps(){
+        return numberOfLamps;
+    }
+    public int getNumberOfWindows(){
+        return numberOfWindows;
+    }
+
+
+
+
 }
