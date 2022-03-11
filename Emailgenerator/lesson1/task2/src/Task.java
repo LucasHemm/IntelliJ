600d5cbab31225874a355f3600d6c0db50af6939
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-03-11 09:53:28 +0100

    update

---
+++ Emailgenerator/lesson1/task2/src/Task.java
@@ -0,0 +1,12 @@
+public class Task {
+    String[] organisations = {"CPHBusiness ", "SpaceX", "Brentford"};
+
+    //Method for generating the second part of an email address
+    /* TODO: skriv metodesignaturen her */
+       public String getOrganisation(int i){
+
+        return null;
+    }
+
+
+}
---
+++ Emailgenerator/lesson1/task3/src/Task.java
@@ -0,0 +1,12 @@
+public class Task {
+String[] organisations = {"CPHBusiness ", "SpaceX", "Brentford"};
+
+    //Method for generating the second part of an email address
+       public String getOrganisation(int i) {
+
+             
+    }
+
+
+
+}
