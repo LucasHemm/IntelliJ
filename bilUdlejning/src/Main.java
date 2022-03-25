efbe733a65f76b4a3007cd13e6f078a6445f24a2
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-03-25 11:04:31 +0100

    Update Main.java

--- bilUdlejning/src/Main.java
+++ bilUdlejning/src/Main.java
@@ -3,21 +3,21 @@ public class Main {
     public static void main(String[] args) {
         FleetOfCars fleet = new FleetOfCars();
 
-        Car F1Ferrari = new GasolineCar(40, "AZ 656 65", "Ferrari", "Formula 1 car", 0);
-        Car W13 = new GasolineCar(19, "1234 89", "Mercedes", "Formula 1 car", 1);
-        Car Tesla = new ElectricCar(100000, 648,"82139 128","Tesla","Sports car",4);
-        Car DieselBandit = new DieselCar(50,true,"bt 7277 89", "Ford","Stationcar",5);
+        Car f1Ferrari = new GasolineCar(40, "AZ 656 65", "Ferrari", "Formula 1 car", 0);
+        Car w13 = new GasolineCar(19, "1234 89", "Mercedes", "Formula 1 car", 1);
+        Car tesla = new ElectricCar(100000, 648,"82139 128","Tesla","Sports car",4);
+        Car dieselBandit = new DieselCar(50,true,"bt 7277 89", "Ford","Stationcar",5);
 
-        //System.out.println(F1Ferrari.getRegistrationFee());
-        //System.out.println(F1Ferrari);
-        fleet.addCar(F1Ferrari);
-        fleet.addCar(W13);
-        fleet.addCar(Tesla);
-        fleet.addCar(DieselBandit);
+        //System.out.println(f1Ferrari.getRegistrationFee());
+        //System.out.println(f1Ferrari);
+        fleet.addCar(f1Ferrari);
+        fleet.addCar(w13);
+        fleet.addCar(tesla);
+        fleet.addCar(dieselBandit);
         //System.out.println(fleet.getTotalFee());
-        //System.out.println(W13);
+        //System.out.println(w13);
         System.out.println(fleet);
         System.out.println(fleet.getTotalFee());
-        //System.out.println(Tesla.getModel());
+        //System.out.println(tesla.getModel());
     }
 }
